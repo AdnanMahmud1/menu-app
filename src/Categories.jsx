90fc0c80c305da76e0495682e99a0cb4107b5544
DESKTOP-NOMKETR\adnan <11adnan@live.com>
2023-05-06 00:47:52 +0600

    filter menu

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,12 +1,19 @@
-
-
-const Categories = ({categories}) => {
+const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
-        {categories.map((category) => {
-            return <button type="button"  className="btn" key={category}>{category}</button>
-        })}
+      {categories.map((category) => {
+        return (
+          <button
+            type="button"
+            className="btn"
+            key={category}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
     </div>
-  )
-}
-export default Categories
+  );
+};
+export default Categories;
